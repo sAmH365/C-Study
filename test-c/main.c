978a906147f848cf76900d56e5ec631aafa865b8
sAmH365 <npower5377@naver.com>
2025-02-01 12:19:07 +0900

    first commit

---
+++ test-c/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  test-c
+//
+//  Created by saehim on 2/1/25.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World\n");
+    int a, b;
+    
+    a = 5;
+    b = 2;
+    
+    printf("%d", a+b);
+    printf("\n");
+    return 0;
+}
